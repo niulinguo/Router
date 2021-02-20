63cf6e4ea3d6f676a3e42054024a5aa1fc3621cd
niulinguo <niulinguo@163.com>
2021-02-20 17:03:06 +0800

    渠道

--- app/src/main/java/com/lingo/router/MyApp.java
+++ app/src/main/java/com/lingo/router/MyApp.java
@@ -1,14 +1,31 @@
 package com.lingo.router;
 
 import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.lingo.router.runtime.Router;
 
 public class MyApp extends Application {
 
+    private static final String TAG = "MyApp";
+
     @Override
     public void onCreate() {
         super.onCreate();
         Router.INSTANCE.init();
+
+        try {
+            final ApplicationInfo info = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            final String mtaChannel = info.metaData.getString("MTA_CHANNEL");
+            printLog("mtaChannel:" + mtaChannel);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printLog(String msg) {
+        Log.i(TAG, msg);
     }
 }
